4ae8591badabc857ec803c539cd99cd01fedf95a
nadlabak <pavel@doshaska.net>
2015-12-03 16:19:41 +0100

    xt926: cmhw: use DisplayColorCalibration for PCC
    Change-Id: I443577f2246c930647479de9abcbc88deac20bae

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,3 +36,8 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 
 # TWRP
 DEVICE_RESOLUTION := 720x1280
+
+# CMHW (for display color calibration - PCC via MDP)
+BOARD_USES_CYANOGEN_HARDWARE := true
+BOARD_HARDWARE_CLASS += hardware/cyanogen/cmhw
+
